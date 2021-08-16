7134c9c504c68a789361c56fa28a83a598a55ea4
Ananthnbhat <ananthnbhat2@gmail.com>
2021-08-16 21:59:56 +0530

    markdown previewer first commit

---
+++ markdown-previewer/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="wrapper">
+      <div id="editor">
+        This is the editor
+      </div>
+      <div id="previewer">
+        This is the previewer
+      </div>
+    </div>
+  );
+}
+
+export default App;
