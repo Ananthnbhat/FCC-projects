584c7b1fad4a9c7a83f4d0b803a639eac8aede44
Ananthnbhat <ananthnbhat2@gmail.com>
2021-08-17 21:19:47 +0530

    add textarea and state in editor

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [editorContent, setEditorConent] = useState()
+
+  const handleChange = event => {
+    setEditorConent(event.target.value)
+  }
   return (
     <div id="wrapper">
       <div id="editor">
-        This is the editor
+        <textarea id="editor-area" value={editorContent} onChange={handleChange}></textarea>
       </div>
       <div id="previewer">
-        This is the previewer
+        {editorContent}
       </div>
     </div>
   );
