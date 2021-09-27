5d770555e4b7cdcba89bd26d238f111c88decaf7
Ananthnbhat <ananthnbhat2@gmail.com>
2021-09-27 10:52:38 +0530

    add logic for session start, stop & reset

--- pomodoro-clock/src/App.jsx
+++ pomodoro-clock/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
-  const [timerType] = useState("Session");
+  const [timerType, setTimerType] = useState("SESSION");
+  const [timerStatus, setTimerStatus] = useState(false);
 
   const reset = () => {
     setBreakLength(5);
@@ -23,6 +24,9 @@ const App = () => {
   const decrementSession = () =>
     setSessionLength(sessionLength > 1 ? sessionLength - 1 : sessionLength);
 
+  const changeTimerStatus = () =>
+    setTimerStatus(timerStatus === false ? true : false);
+
   return (
     <div id="container">
       <div id="title">25 + 5 clock</div>
@@ -33,8 +37,15 @@ const App = () => {
         decrementSession={decrementSession}
         decrementBreak={decrementBreak}
         incrementBreak={incrementBreak}
+        timerStatus={timerStatus}
       />
-      <Timer reset={reset} timerType={timerType} />
+      <Timer
+        reset={reset}
+        timerType={timerType}
+        breakLength={breakLength}
+        sessionLength={sessionLength}
+        changeTimerStatus={changeTimerStatus}
+      />
     </div>
   );
 };
@@ -45,12 +56,20 @@ const Config = (props) => {
       <div id="break-config" className="config">
         <label id="break-label">Break Length</label>
         <div>
-          <button id="break-decrement" onClick={props.decrementBreak}>
+          <button
+            id="break-decrement"
+            onClick={props.decrementBreak}
+            disabled={props.timerStatus}
+          >
             -
           </button>
           &nbsp;
           <p id="break-length">{props.breakLength}</p>&nbsp;
-          <button id="break-increment" onClick={props.incrementBreak}>
+          <button
+            id="break-increment"
+            onClick={props.incrementBreak}
+            disabled={props.timerStatus}
+          >
             +
           </button>
         </div>
@@ -58,12 +77,20 @@ const Config = (props) => {
       <div id="session-config" className="config">
         <label id="session-label">Session Length</label>
         <div>
-          <button id="session-decrement" onClick={props.decrementSession}>
+          <button
+            id="session-decrement"
+            onClick={props.decrementSession}
+            disabled={props.timerStatus}
+          >
             -
           </button>
           &nbsp;
           <p id="session-length">{props.sessionLength}</p>&nbsp;
-          <button id="session-increment" onClick={props.incrementSession}>
+          <button
+            id="session-increment"
+            onClick={props.incrementSession}
+            disabled={props.timerStatus}
+          >
             +
           </button>
         </div>
@@ -73,25 +100,84 @@ const Config = (props) => {
 };
 
 const Timer = (props) => {
-  const [startStopText, setStartStopText] = useState("START");
+  const [startStop, setStartStop] = useState("START");
+  const [sessionMin, setSessionMin] = useState(props.sessionLength);
+  const [sessionSec, setSessionSec] = useState(0);
+  const [breakMin, setBreakMin] = useState(props.breakLength);
+  const [breakSec, setBreakSec] = useState(0);
 
   const handleStartStop = () => {
-    startStopText === "START"
-      ? setStartStopText("PAUSE")
-      : setStartStopText("START");
+    if (startStop === "START") {
+      setStartStop("PAUSE");
+      props.changeTimerStatus();
+    } else {
+      setStartStop("START");
+      props.changeTimerStatus();
+    }
   };
+
+  const handleReset = () => {
+    if (startStop === "PAUSE") handleStartStop();
+    props.reset();
+    if (sessionMin != props.sessionLength) resetSession();
+  };
+
+  const resetSession = () => {
+    setSessionMin(props.sessionLength);
+    setSessionSec(0);
+  };
+
+  useEffect(() => {
+    if (startStop === "START") {
+      resetSession();
+    }
+    // eslint-disable-line react-hooks/exhaustive-deps
+  }, [props.sessionLength]);
+
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (sessionSec > 0) {
+        setSessionSec(sessionSec - 1);
+      }
+      if (sessionSec === 0) {
+        if (sessionMin === 0) {
+          clearInterval(myInterval);
+        } else {
+          setSessionMin(sessionMin - 1);
+          setSessionSec(59);
+        }
+      }
+    }, 1000);
+    if (startStop === "START") {
+      clearInterval(myInterval);
+      return;
+    }
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
 
   return (
     <div>
       <div id="timer">
         <div id="timer-label">{props.timerType}</div>
-        <div id="time-left">25:00</div>
+        {props.timerType === "SESSION" ? (
+          <div id="time-left">
+            {sessionMin < 10 ? `0${sessionMin}` : sessionMin}:
+            {sessionSec < 10 ? `0${sessionSec}` : sessionSec}
+          </div>
+        ) : (
+          <div id="time-left">
+            {breakMin < 10 ? `0${breakMin}` : breakMin}:
+            {breakSec < 10 ? `0${breakSec}` : breakSec}
+          </div>
+        )}
       </div>
       <div id="timer-control">
         <button id="start_stop" onClick={handleStartStop}>
-          {startStopText}
+          {startStop}
         </button>
-        <button id="reset" onClick={props.reset}>
+        <button id="reset" onClick={handleReset}>
           RESET
         </button>
       </div>
